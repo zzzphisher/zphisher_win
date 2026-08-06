880ffc70ca4dfb434a69fbbe7cdb092c03bfd20f
zzzphisher <buzzstudio832@gmail.com>
2026-08-07 00:23:27 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,11 +3,11 @@ $ErrorActionPreference = "Stop"
 # Configuration
 $ExeUrl = "https://raw.githubusercontent.com/zzzphisher/zphisher_win/main/zphisher.exe"
 $InstallDir = "$env:LOCALAPPDATA\Microsoft\WindowsApps"
-$TargetFile = "$InstallDir\toolbuz.exe"
+$TargetFile = "$InstallDir\zhpisher.exe"
 
 Write-Host "Installation de toolbuz en cours..." -ForegroundColor Cyan
 
 # 1. Téléchargement du fichier .exe
 Invoke-WebRequest -Uri $ExeUrl -OutFile $TargetFile
 
-Write-Host "Succès ! Tu peux maintenant taper 'toolbuz' dans n'importe quel terminal." -ForegroundColor Green
+Write-Host "Succès ! Tu peux maintenant taper 'zphisher' dans n'importe quel terminal." -ForegroundColor Green
